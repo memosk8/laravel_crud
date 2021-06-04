bebb837070d7aeec81b7f55ba625d1f82d2ff649
memosk8 <memolopez9363@gmail.com>
2021-06-04 14:30:44 -0500

    index function can access to DB for products table and display productos view

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -14,7 +14,8 @@ class ProductoController extends Controller
      */
     public function index()
     {
-        //
+        $productos = Producto::all();
+        return view("productos.index", compact('productos'));
     }
 
     /**
