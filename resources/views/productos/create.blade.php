d8bb59e46fb64990ba8bfcc5893d8e3d3d3afd4d
memosk8 <memolopez9363@gmail.com>
2021-06-05 15:11:54 -0500

    now extending from master layout to apply styles

--- resources/views/productos/create.blade.php
+++ resources/views/productos/create.blade.php
@@ -1,31 +1,44 @@
-<h3>Agregar producto</h3>
+@extends('layout.layout')
 
-<form action="{{ route('productos.store')}}" method="POST">
-   @csrf
+@section('title', 'Agregar Productos')
 
-   <div>
-      <label for="">Producto: </label>
-      <input type="text" name="name" id="name" required maxlength="100">
-   </div>
-   <div>
-      <label for="">Descripción: </label>
-      <textarea name="description" id="" cols="30" rows="5"></textarea>
-   </div>
-   <div>
-      <label for="">Precio: </label>
-      <input type="text" name="price" id="price" required maxlength="7">
-   </div>
-   <div>
-      <label for="">Fecha de caducidad: </label>
-      <input type="date" name="expiration">
-   </div>
-   <div>
-      <label for="">En stock: </label>
-      <input type="number" name="stock" id="" min="1" max="1000">
-   </div>
-   <div>
-      <label for="">Peso en Kg: </label>
-      <input type="text" name="weightKg" id="" required>
-   </div>
-   <input type="submit" value="Guardar Producto">
-</form>
+@section('content')
+
+   
+
+   <form action="{{ route('productos.store')}}" method="POST">
+      <h1 class="form">Agregar producto</h1>
+      @csrf
+
+      <div>
+         <label for="">Producto: </label>
+         <input type="text" name="name" id="name" required maxlength="100">
+      </div>
+      <div>
+         <label for="">Descripción: </label>
+         <textarea name="description" id="" cols="30" rows="5"></textarea>
+      </div>
+      <div>
+         <label for="">Precio: </label>
+         <input type="text" name="price" id="price" required maxlength="7">
+      </div>
+      <div>
+         <label for="">Fecha de caducidad: </label>
+         <input type="date" name="expiration">
+      </div>
+      <div>
+         <label for="">En stock: </label>
+         <input type="number" name="stock" id="" min="1" max="1000">
+      </div>
+      <div>
+         <label for="">Peso en Kg: </label>
+         <input type="text" name="weightKg" id="" required>
+      </div>
+      <div>
+         <label for="">Link de imagen: </label>
+         <input type="text" name="imgLink" id="" >
+      </div>
+      <input type="submit" value="Guardar Producto">
+   </form>
+
+@endsection
